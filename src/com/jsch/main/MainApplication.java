2ffad6b3a48d7f41b859b9fd096dd04bbce0fbd6
zhang xinhang <504266561@qq.com>
2017-01-15 18:32:17 +0800

    change

--- src/com/jsch/main/MainApplication.java
+++ src/com/jsch/main/MainApplication.java
@@ -13,31 +13,42 @@ public class MainApplication {
 		StringBuilder sb = new StringBuilder();
 		String str = out;
 		int index = str.indexOf("dis");
-		if (index > -1) {
-			String newStr = str.substring(str.indexOf("dis"), str.length());
-			int h3cSoftVersionIndex = newStr.indexOf("Comware Software");
-			int hwSoftVersionIndex = newStr.indexOf("VRP (R) software");
-			if (h3cSoftVersionIndex > -1) {
-				sb.append(newStr.substring(h3cSoftVersionIndex, newStr.indexOf("\n", h3cSoftVersionIndex)));
-				sb.append(" | ");
-			}
-			if (hwSoftVersionIndex > -1) {
-				sb.append(newStr.substring(h3cSoftVersionIndex, newStr.indexOf("\n", hwSoftVersionIndex)));
-				sb.append(" | ");
-			}
+		try {
+			if (index > -1) {
+				String newStr = str.substring(str.indexOf("dis"), str.length());
+				int h3cSoftVersionIndex = newStr.indexOf("Comware Software");
+				int hwSoftVersionIndex = newStr.indexOf("VRP (R) software");
+				if (h3cSoftVersionIndex > -1) {
+					sb.append(newStr.substring(h3cSoftVersionIndex + 18, newStr.indexOf("\n", h3cSoftVersionIndex)));
+					sb.append(" | ");
+				}
+				if (hwSoftVersionIndex > -1) {
+					sb.append(newStr.substring(hwSoftVersionIndex + 18, newStr.indexOf("\n", hwSoftVersionIndex)));
+					sb.append(" | ");
+				}
 
-			int h3cVersionIndex = newStr.indexOf("H3C S");
-			int hwVersionIndex = newStr.indexOf("Quidway S");
-			int updateIndex = newStr.indexOf("uptime");
-			if (h3cVersionIndex > -1 && updateIndex > -1) {
-				sb.append(newStr.substring(h3cVersionIndex, updateIndex));
-				sb.append(" | ");
-			}
+				int h3cVersionIndex = newStr.indexOf("H3C S");
+				int hwVersionIndex = newStr.indexOf("Quidway S");
+				int huaweiVersionIndex = newStr.indexOf("HUAWEI S");
+				int updateIndex = newStr.indexOf("uptime");
+				if (h3cVersionIndex > -1 && updateIndex > -1) {
+					sb.append(newStr.substring(h3cVersionIndex, updateIndex));
+					sb.append(" | ");
+				}
 
-			if (hwVersionIndex > -1 && updateIndex > -1) {
-				sb.append(newStr.substring(hwVersionIndex, updateIndex));
-				sb.append(" | ");
+				if (hwVersionIndex > -1 && updateIndex > -1) {
+					sb.append(newStr.substring(hwVersionIndex, updateIndex));
+					sb.append(" | ");
+				}
+				if (huaweiVersionIndex > -1 && updateIndex > -1) {
+					sb.append(newStr.substring(huaweiVersionIndex, updateIndex));
+					sb.append(" | ");
+				}
+
 			}
+		} catch (Exception e) {
+			System.out.println("origin:" + out);
+			e.printStackTrace();
 		}
 		return sb.toString();
 	}
@@ -52,7 +63,8 @@ public class MainApplication {
 		System.out.println("***************begin:0,sum:" + size + "************************");
 		for (String[] strings : list) {
 			System.out.println("***************************************************************************");
-			System.out.println("***************begin:" + (++i) + ",sum:" + size + "***********************************************");
+			System.out.println("***************begin:" + (++i) + ",sum:" + size
+					+ "***********************************************");
 			System.out.println("***************************************************************************");
 			String out = "";
 			if (strings[0] != null && !"".equals(strings[0])) {
@@ -62,7 +74,7 @@ public class MainApplication {
 				String sshVersion = strings[3].trim();
 				System.out.println(" begin host:" + host);
 				// out = ShellUtils.execCmd(cmd, user, passwd, host);
-				out = SSHClient.execute(host, user, passwd, "dis ver");
+				out = SSHClient.execute(host, user, passwd, "display version");
 				String[] retArr = handleOutputString(out).split(" \\| ");
 				String version1 = retArr[0];
 				String version2 = "";
